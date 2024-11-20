d3ee5d70c9633f9f520ee3179a5dc74a7d0064ce
Blake Torrey <btorrey1998@gmail.com>
2024-11-19 20:00:44 -0700

    added three pages, Login, Home, and the Error page. Also created react Dom routing for those three pages on the main.tsx client file.

---
+++ client/src/Pages/Login.tsx
@@ -0,0 +1,14 @@
+
+
+const Login = () => {
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Login;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,35 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ErrorPage from './Pages/ErrorPage.tsx';
+import Home from './Pages/Home.tsx';
+import Login from './Pages/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ]
+  }
+]);
+
+const rootElement = document.getElementById('root');
+if(rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
