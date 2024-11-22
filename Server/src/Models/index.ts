afd790fa490dac040b7ff92adf4bad124d358512
Blake Torrey <btorrey1998@gmail.com>
2024-11-21 21:02:13 -0700

    Added authorization for login on the client and serverside, this includes routing and middleware. Also created a model for Users and made some edits to the main page to reflect authorization and the users.

---
+++ Server/src/Models/index.ts
@@ -0,0 +1,14 @@
+import sequelize from '../config/connection.js';
+import { UserFactory } from './user.js';
+import { TransactionFactory } from './transaction.js';
+
+const User = UserFactory(sequelize);
+const Transaction = TransactionFactory(sequelize);
+
+User.hasMany(Transaction, {
+    onDelete: 'CASCADE',
+});
+
+Transaction.belongsTo(User);
+
+export { sequelize, User, Transaction};
