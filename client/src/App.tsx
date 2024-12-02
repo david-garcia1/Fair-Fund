80314d4d52c671ec26a7df8662caeaf41af134eb
Blake Torrey <btorrey1998@gmail.com>
2024-12-02 12:43:44 -0700

    page routing edit for deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "../../client/src/Pages/home";
+import HomePage from "./client/src/Pages/home";
 import Navbar from "./Components/Navbar.tsx";
-import MarketPage from "../../client/src/Pages/market.tsx";
+import MarketPage from "./client/src/Pages/market.tsx";
 import Login from './Components/loginComponent';
 import Register from "./Components/Register.tsx";
 import ProtectedRoute from "./Components/AuthContext/ProtectedRoute.tsx";
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -18,26 +18,3 @@ root.render(
   </React.StrictMode>
 )
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         index: true,
-//         element: <Home />,
-//       },
-//       {
-//         path: '/login',
-//         element: <Login />,
-//       },
-//     ]
-//   }
-// ]);
-
-// const rootElement = document.getElementById('root');
-// if(rootElement) {
-//   ReactDOM.createRoot(rootElement).render(
-//   <RouterProvider router={router} />);
-// }
